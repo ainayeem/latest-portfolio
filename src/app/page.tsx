05ddd96e006b0fea583292e63755a375eecf63be
ainayeem-pc <ain.nayeem1999@gmail.com>
2025-04-06 17:49:48 +0600

    feat: ✨ add picture in hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/floating-navbar";
+
+const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
 
 export default function Home() {
   return (
-    <div className="bg-custom-black-100">
-      <Hero />
-    </div>
+    <main className="relative bg-custom-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        {/* <Grid />
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Approach />
+        <Footer /> */}
+      </div>
+    </main>
   );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,19 +1,18 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { FaLocationArrow } from "react-icons/fa";
+import { Ripple } from "./magicui/ripple";
+import MainButton from "./ui/MainButton";
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/text-generate-effect";
 
 const Hero = () => {
   return (
     <div className="">
       <div>
         {/* spotlight */}
-        <Spotlight
-          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
-          fill="white"
-        />
-        <Spotlight
-          className="h-[80vh] w-[50vw] top-10 left-full"
-          fill="purple"
-        />
+        <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
+        <Spotlight className="h-[80vh] w-[50vw] top-10 left-full" fill="purple" />
         <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
       </div>
       {/*  */}
@@ -28,9 +27,44 @@ const Hero = () => {
         />
         {/* Radial gradient for the container to give a faded look */}
         <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-custom-black-100 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
-        <p className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-4xl font-bold text-transparent sm:text-7xl">
-          Backgrounds
-        </p>
+
+        {/* info */}
+        <div className="lg:flex items-center justify-between">
+          <div className="flex justify-center relative my-20 z-10">
+            <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+              <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">Web Developer</p>
+
+              <TextGenerateEffect
+                duration={2}
+                filter={false}
+                words="Building Digital Worlds One Pixel and Line of Code at a Time"
+                className="text-center text-[40px] md:text-5xl lg:text-6xl"
+              />
+
+              <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">Hi! I&apos;m Ashikul Islam Nayeem.</p>
+              <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-xl">
+                With a great passion for the technological world, both Frontend and Backend, pursuing new challenges and continually exploring new
+                trends.
+              </p>
+
+              <a href="#about">
+                <MainButton title="View CV" icon={<FaLocationArrow />} position="right" />
+              </a>
+            </div>
+          </div>
+          <div className="flex-shrink-0 w-full max-w-sm lg:max-w-md xl:max-w-lg">
+            <div className="relative aspect-square flex items-center justify-center">
+              <Ripple className="absolute inset-0 rounded-full opacity-70 scale-110" />
+              <Image
+                src="https://www.mockofun.com/wp-content/uploads/2019/12/circle-profile-pic.jpg"
+                alt="Profile"
+                width={400}
+                height={400}
+                className="relative z-10 rounded-full object-cover border-4 border-indigo-500 shadow-xl"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
